e40ff588cb3878a0fae7ffff711195ddeac72e64
Vitalii Vovk <99590889+vovkvi@users.noreply.github.com>
2022-02-26 16:25:26 +0300

    Add files via upload

---
+++ contact_form/contact_form.php
@@ -0,0 +1,81 @@
+<?php
+
+// configure
+$from = 'Contact form <yourmail@domain.com>';
+$sendTo = 'vetalvovk@gmail.com';
+$subject = 'New message from contact form';
+$fields = array('name' => 'Name', 'email' => 'Email', 'message' => 'Message'); // array variable name => Text to appear in the email
+$okMessage = 'Сообщение отправлено! Я отвечу на Вам в ближайщее время.';
+$errorMessage = 'Произошла ошибка. Пожалуйста повторите попытку позже.';
+
+// let's do the sending
+
+if(isset($_POST['g-recaptcha-response']) && !empty($_POST['g-recaptcha-response'])):
+    //your site secret key
+    $secret = '6LeVAIUeAAAAAHzZJGR7HNVVDdT9jECUlGUBU-36';
+    //get verify response data
+
+    $c = curl_init('https://www.google.com/recaptcha/api/siteverify?secret='.$secret.'&response='.$_POST['g-recaptcha-response']);
+    curl_setopt($c, CURLOPT_RETURNTRANSFER, 1);
+    $verifyResponse = curl_exec($c);
+
+    $responseData = json_decode($verifyResponse);
+    if($responseData->success):
+
+        try
+        {
+            $emailText = nl2br("You have new message from Contact Form\n");
+
+            foreach ($_POST as $key => $value) {
+
+                if (isset($fields[$key])) {
+                    $emailText .= nl2br("$fields[$key]: $value\n");
+                }
+            }
+
+            $headers = array('Content-Type: text/html; charset="UTF-8";',
+                'From: ' . $from,
+                'Reply-To: ' . $from,
+                'Return-Path: ' . $from,
+            );
+            
+            mail($sendTo, $subject, $emailText, implode("\n", $headers));
+
+            $responseArray = array('type' => 'success', 'message' => $okMessage);
+        }
+        catch (\Exception $e)
+        {
+            $responseArray = array('type' => 'danger', 'message' => $errorMessage);
+        }
+
+        if (!empty($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest') {
+            $encoded = json_encode($responseArray);
+
+            header('Content-Type: application/json');
+
+            echo $encoded;
+        }
+        else {
+            echo $responseArray['message'];
+        }
+
+    else:
+        $errorMessage = 'Robot verification failed, please try again.';
+        $responseArray = array('type' => 'danger', 'message' => $errorMessage);
+        $encoded = json_encode($responseArray);
+
+            header('Content-Type: application/json');
+
+            echo $encoded;
+    endif;
+else:
+    $errorMessage = 'Please click on the reCAPTCHA box.';
+    $responseArray = array('type' => 'danger', 'message' => $errorMessage);
+    $encoded = json_encode($responseArray);
+
+            header('Content-Type: application/json');
+
+            echo $encoded;
+endif;
+
+?>
---
+++ js/main.js
@@ -0,0 +1,140 @@
+
+(function($) {
+"use strict";
+    // Subpages resize
+    function subpages_resize() {
+        var subpagesHeight = $('.pt-page-current').height();
+        $(".subpages").height(subpagesHeight + 50);
+    }
+
+    // Contact form validator
+    $(function () {
+
+        $('#contact-form').validator();
+
+        $('#contact-form').on('submit', function (e) {
+            if (!e.isDefaultPrevented()) {
+                var url = "contact_form/contact_form.php";
+
+                $.ajax({
+                    type: "POST",
+                    url: url,
+                    data: $(this).serialize(),
+                    success: function (data)
+                    {
+                        var messageAlert = 'alert-' + data.type;
+                        var messageText = data.message;
+
+                        var alertBox = '<div class="alert ' + messageAlert + ' alert-dismissable"><button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>' + messageText + '</div>';
+                        if (messageAlert && messageText) {
+                            $('#contact-form').find('.messages').html(alertBox);
+                            $('#contact-form')[0].reset();
+                        }
+                    }
+                });
+                return false;
+            }
+        });
+    });
+    // /Contact form validator
+
+    // Hide Mobile menu
+    function mobileMenuHide() {
+        var windowWidth = $(window).width();
+        if (windowWidth < 1024) {
+            $('#site_header').addClass('mobile-menu-hide');
+        }
+    }
+    // /Hide Mobile menu
+
+    //On Window load & Resize
+    $(window)
+        .on('load', function() { //Load
+            // Animation on Page Loading
+            $(".preloader").fadeOut("slow");
+
+            // initializing page transition.
+            var ptPage = $('.subpages');
+            if (ptPage[0]) {
+                PageTransitions.init({
+                    menu: 'ul.site-main-menu',
+                });
+            }
+        })
+        .on('resize', function() { //Resize
+             mobileMenuHide();
+
+             setTimeout(function(){
+                subpages_resize();
+            }, 500);
+        })
+        .scroll(function () {
+            if ($(window).scrollTop() < 20) {
+                $('.header').removeClass('sticked');
+            } else {
+                $('.header').addClass('sticked');
+            }
+        })
+        .scrollTop(0);
+
+
+    // On Document Load
+    $(document).on('ready', function() {
+        // Mobile menu
+        $('.menu-toggle').on("click", function () {
+            $('#site_header').toggleClass('mobile-menu-hide');
+        });
+
+        // Mobile menu hide on main menu item click
+        $('.site-main-menu').on("click", "a", function (e) {
+            mobileMenuHide();
+        });
+
+        // Sidebar toggle
+        $('.sidebar-toggle').on("click", function () {
+            $('#blog-sidebar').toggleClass('open');
+        });
+		
+        // Text rotation
+        $('.text-rotation').owlCarousel({
+            loop: true,
+            dots: false,
+            nav: false,
+            margin: 0,
+            items: 1,
+            autoplay: true,
+            autoplayHoverPause: false,
+            autoplayTimeout: 3800,
+            animateOut: 'zoomOut',
+            animateIn: 'zoomIn'
+        });
+
+        $('.ajax-page-load-link').magnificPopup({
+            type: 'ajax',
+            removalDelay: 300,
+            mainClass: 'mfp-fade',
+            gallery: {
+                enabled: true
+            },
+        });
+
+        //Form Controls
+        $('.form-control')
+            .val('')
+            .on("focusin", function(){
+                $(this).parent('.form-group').addClass('form-group-focus');
+            })
+            .on("focusout", function(){
+                if($(this).val().length === 0) {
+                    $(this).parent('.form-group').removeClass('form-group-focus');
+                }
+            });
+
+        //Google Maps
+        $("#map").googleMap();
+        $("#map").addMarker({
+            address: "Saint Petersburg Russia" // Your Address
+        });
+    });
+
+})(jQuery);
