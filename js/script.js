4be84b10aa69399ac046c3001990be3ccd8a03b4
Vitalii Vovk <99590889+vovkvi@users.noreply.github.com>
2023-07-15 13:13:17 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,7 +1,21 @@
+document.getElementsByClassName("copy")[0].innerHTML = '&copy; Vitalii Vovk, ' + new Date().getFullYear();
+
 // Animations
 AOS.init({
   anchorPlacement: 'top-left',
   duration: 1000
 });
 
-// Add your javascript here
+// Map
+const osm_default = L.tileLayer(
+	'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+	{
+		attribution: '<a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+		maxZoom: 18
+	});		
+const mymap = L.map('map', {
+	center: [60.05126, 30.33233],
+	zoom: 9,
+	layers: [osm_default]
+});	
+L.marker([60.05126, 30.33233]).addTo(mymap).bindPopup('Сейчас я здесь :)');
